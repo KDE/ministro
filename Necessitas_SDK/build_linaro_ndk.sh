d2b48557fc1c194bf23c84fcaf3110390281dfe2
Ray Donnelly <mingw.android@laptop.com>
2011-10-17 21:30:11 +0100

    Work around windows 260 character filepath limit

--- Necessitas_SDK/build_linaro_ndk.sh
+++ Necessitas_SDK/build_linaro_ndk.sh
@@ -125,7 +125,7 @@ echo NDK is $NDK
 echo NDK is $NDK
 echo NDK is $NDK
     if [ ! -f $ROOTDIR/${ARCH_ABI}-${GCC_VER}-${BUILD_NDK}.tar.bz2 ]; then
-        $NDK/build/tools/rebuild-all-prebuilt.sh --arch=$ARCH --patches-dir=/tmp/ndk-tc-patches --build-dir=$ROOTDIR/ndk-toolchain-${BUILD}-build-tmp --verbose --package-dir=$ROOTDIR --gcc-version=$GCC_VER --gdb-path=$GDB_ROOT_PATH --gdb-version=$GDB_VER --mpfr-version=2.4.2 --gmp-version=4.3.2 --binutils-version=2.20.1 --toolchain-src-dir=$TCSRC --gdb-with-python=$PYTHONVER
+	$NDK/build/tools/rebuild-all-prebuilt.sh --arch=$ARCH --patches-dir=/tmp/ndk-tc-patches --build-dir=/usr/ndkb --verbose --package-dir=$ROOTDIR --gcc-version=$GCC_VER --gdb-path=$GDB_ROOT_PATH --gdb-version=$GDB_VER --mpfr-version=2.4.2 --gmp-version=4.3.2 --binutils-version=2.20.1 --toolchain-src-dir=$TCSRC --gdb-with-python=$PYTHONVER
     else
         echo "Skipping NDK build, already done."
         echo $ROOTDIR/${ARCH_ABI}-${GCC_VER}-${BUILD_NDK}.tar.bz2
