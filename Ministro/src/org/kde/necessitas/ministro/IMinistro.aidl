ccd536e3ce896cc7f57d19cce3a2279741dc2a6e
BogDan Vatra <bogdan@kde.org>
2013-03-20 20:26:08 +0200

    Update copyright header

--- Ministro/src/org/kde/necessitas/ministro/IMinistro.aidl
+++ Ministro/src/org/kde/necessitas/ministro/IMinistro.aidl
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2011, BogDan Vatra <bog_dan_ro@yahoo.com>
+    Copyright (c) 2011-20013, BogDan Vatra <bogdan@kde.org>
 
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions
--- Ministro/src/org/kde/necessitas/ministro/Library.java
+++ Ministro/src/org/kde/necessitas/ministro/Library.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2011, BogDan Vatra <bog_dan_ro@yahoo.com>
+    Copyright (c) 2011-20013, BogDan Vatra <bogdan@kde.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
