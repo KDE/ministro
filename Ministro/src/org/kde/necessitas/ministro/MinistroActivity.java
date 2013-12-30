98d8e546ee1454a415d0ce35d0e76d9495419c00
BogDan Vatra <bogdan@kde.org>
2013-12-30 16:27:06 +0200

    Update copyright notice

--- Ministro/src/org/kde/necessitas/ministro/MinistroActivity.java
+++ Ministro/src/org/kde/necessitas/ministro/MinistroActivity.java
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2011-20013, BogDan Vatra <bogdan@kde.org>
+    Copyright (c) 2011-2014, BogDan Vatra <bogdan@kde.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
